088ab60942b8ed33310c4e89b9bdd8cdbefac429
chromebook <aaugustinwwezel@gmail.com>
2024-01-01 15:08:42 +0100

    spaces

--- lua/augustin/plugins/colorscheme.lua
+++ lua/augustin/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
-	'rose-pine/neovim', 
-	name = 'rose-pine', 
+	'rose-pine/neovim',
+	name = 'rose-pine',
 	config = function()
 		require('rose-pine').setup({ disable_italics = true, })
 		vim.cmd('colorscheme rose-pine')
