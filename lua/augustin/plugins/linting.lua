19f4fdf4a15cefa06754e276822544a1e0d1915d
Ledugus <aaugustinwwezel@gmail.com>
2025-01-13 14:18:27 +0100

    remove svelte from linters

--- lua/augustin/plugins/linting.lua
+++ lua/augustin/plugins/linting.lua
@@ -9,7 +9,6 @@ return {
 		lint.linters_by_ft = {
 			python = { "pylint" },
 			latex = { "vale" },
-			svelte = { "svelte" },
 		}
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
